9b9eca03e66238ecb6e2b2ddc9506ff800e6bbe4
Alexandra Romano <alex.romano80@gmail.com>
2023-05-28 20:24:29 -0700

    wishlist input working to show displayed text

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -2,25 +2,33 @@ import movieData from "../data/movieData";
 import React, {useState} from "react"
 
 function Wishlist() {
-
-
-        const [inputValue, setInputValue] = useState('');
+const [inputValue, setInputValue] = useState('');
+const [wishList, setWishList] = useState([])
       
     
-        function handleChange(event) {
-          const insertValue = event.target.value;
-          setInputValue(insertValue);
-          console.log(insertValue)
-        }
+function handleChange(event) {
+const insertValue = event.target.value;
+setInputValue(insertValue);
+console.log(insertValue)
+}
     
 function handleSubmit(event) {
 event.preventDefault();
-console.log("form submit")
+addMovies(inputValue)
+setInputValue('')
+}
+
+const addMovies = (newmovie) => {
+setWishList([...wishList, newmovie])
 }
 
     return (
         <div>
         <h1>My Wishlist</h1>
+        <section>{wishList.map((movie, index) => (
+            <p key={index}>{movie}</p>
+        ))}</section>
+        <h1>Create Your Wishlist</h1>
         <form onSubmit={handleSubmit}>
         <input type="text" value={inputValue} placeholder="Search a movie" onChange={handleChange} />
 
