a395100a7183e5100854235171475ea19d27902c
Alexandra Romano <alex.romano80@gmail.com>
2023-05-29 15:37:05 -0700

    added useEffect and loaded data

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 // import { Route, Switch } from "react-router-dom";
 import movieData from "../data/movieData"
 import Header from "../components/Header";
@@ -8,30 +8,27 @@ import Wishlist from "./Wishlist";
 
 
 function App() {
+const [fetchMovies, setFetchMovies] = useState([])
 
-
+useEffect(() => {
+    fetch('http://localhost:3000/posts')
+    .then((res) => (res.json())
+    .then (data => setFetchMovies(data))
+  )}, []);
 
 const headerTitle = movieData.name
 const about = movieData.about
 const image = console.log(movieData.image)
 
-const moviePost = movieData.posts.map((title, index) => (
-  <ul>
-  <li key={index}>{title.title}</li>
-  <li key={index}>{title.preview}</li>
-  <li key={index}>{title.cast}</li>
-  <li key={index}>{title.year}</li>
-  <br></br>
-  <img key={index} src={title.image}></img>
-  </ul>
-));
+
+console.log(fetchMovies)
 
 
   return (
     <div className="App">
       <NavBar image={image} />
      
-        <Movies moviePost={moviePost} />
+        <Movies fetchMovies={fetchMovies} />
      
         <Wishlist />
     
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,4 +1,15 @@
-function Movies({moviePost}) {
+function Movies({fetchMovies}) {
+
+    const moviePost = fetchMovies.map((title, index) => (
+        <ul>
+        <li key={index}>{title.title}</li>
+        <li key={index}>{title.preview}</li>
+        <li key={index}>{title.cast}</li>
+        <li key={index}>{title.year}</li>
+        <br></br>
+        <img key={index} src={title.image}></img>
+        </ul>
+      ));
     return (
         <div>
         <h1>Movies</h1>
