f46fdfbc611581fb23a8c9fd7572804a64633a65
Alexandra Romano <alex.romano80@gmail.com>
2023-06-09 18:09:51 -0700

    fixed alignment of wishlist button

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -94,21 +94,23 @@ function Wishlist({ fetchMovies, login }) {
 
       <h1>Create Your Wishlist</h1>
 
-      <form onSubmit={handleSubmit}>
-
+      <form onSubmit={handleSubmit} style={{ display: "flex" }}>
+      <div style={{ flex: 1 }}>
          <Autocomplete
 disablePortal
 id="combo-box-demo"
 options={movieMap}
 onChange={handleChange}
-sx={{ backgroundColor: "white", width: 200, margin: '0 auto' }}
+sx={{ backgroundColor: "white", width: 200, margin: '0 auto', marginLeft: '580px' }}
 renderInput={(params) => <TextField {...params} label="Movies" />}
 /> 
+</div>
         <button className="add" type="submit">
           Add to Wishlist
         </button>
       </form>
     </div>
+    
   );
 }
 
